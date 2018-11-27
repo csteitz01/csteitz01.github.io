f9785dc7c492b0bcaea4c966ed7e546a6e5a3091
Chris <csteitz01@bellarmine.edu>
2018-11-26 21:25:57 -0500

    initial commit

---
+++ project_flickr/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  
+ $('form').submit(function (evt) {
+    evt.preventDefault();
+    var $searchField = $('#search');
+    var $submitButton = $('#submit');
+    $searchField.prop("disabled",true);
+    $submitButton.attr("disabled", true).val("searching....");
+   // Ajax part
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $searchField.val();
+    var flickrOptions = {
+        tags: animal,
+        format: "json"
+      };
+    function displayPhotos(data){
+      var photoHTML = '<ul>';
+        $.each(data.items,function(i,photo) {
+          photoHTML += '<li class="grid-25 tablet-grid-50">';
+          photoHTML += '<a href="' + photo.link + '" class="image">';
+          photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+        }); // end each
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
+      $searchField.prop("disabled", false);
+      $submitButton.attr("disabled", false).val("Search");
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+  }); // end click
+
+}); // end ready
