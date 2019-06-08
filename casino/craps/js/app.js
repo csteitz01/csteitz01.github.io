4ffc5d940f3d9822de69324a48bea498721ff881
csteitz01 <csteitz01@bellarmine.edu>
2019-06-08 10:02:10 -0400

    spaced things out better and added dice roll total

--- casino/craps/js/app.js
+++ casino/craps/js/app.js
@@ -1,27 +1,17 @@
 function rollDice() {
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
-    var status = document.getElementById("status")
     var d1 =  Math.floor(Math.random()*6) + 1;
     var d2 =  Math.floor(Math.random()*6) + 1;
     var diceTotal = d1 + d2;
     die1.innerHTML = d1;
     die2.innerHTML = d2;
-    status.innerHTML - "You rolled " +diceTotal+ "."
+    replace(diceTotal);
 }
 
-function showTotal() {
-let diceTotal = rollDice.diceTotal;
-
-    let message = "" + diceTotal + " was rolled.";
-
-    $('.dieButton #button').click(function() {
-        $('#status').text(message);
-        $('#status').show;
-    });
+function replace(diceTotal) {
+    $("#status p").text(diceTotal);
 }
-
-
 
 document.getElementById('rules').addEventListener('click',
 function() {
