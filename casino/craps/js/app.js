02fb03046cae8829e7528eb329128fef140400cf
csteitz01 <csteitz01@bellarmine.edu>
2019-06-20 16:44:17 -0400

    changed dice and added input fields

--- casino/craps/js/app.js
+++ casino/craps/js/app.js
@@ -1,17 +1,29 @@
 function rollDice() {
-    var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
     var d1 =  Math.floor(Math.random()*6) + 1;
     var d2 =  Math.floor(Math.random()*6) + 1;
     var diceTotal = d1 + d2;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
+    var dieOneImage = "img/dice"+d1+".png"
+    var dieTwoImage = "img/dice"+d2+".png"
     replace(diceTotal);
+    document.querySelector('.d1').setAttribute('src', dieOneImage);
+    document.querySelector('.d6').setAttribute('src', dieTwoImage);
 }
 
 function replace(diceTotal) {
-    $("#status p").text(diceTotal);
+    $("#status h3").text(diceTotal);
 }
+
+document.addEventListener('#bank submit', (e) => {
+    let num = input.value;
+    if (num < 1) {
+        alert('You need to have more than $1 if you want to play');
+    }
+    else {
+        document.querySelector('#bank input').style.display = 'none';
+        document.querySelector('#bank button').style.display = 'none';
+        $("h2 span").text(num);
+    }
+});
 
 document.getElementById('rules').addEventListener('click',
 function() {
